db312fcd1135e5a3a2992e2280326d4f0114808d
Edwin Cheng <edwincheng3@gmail.com>
2021-02-21 04:57:18 -0800

    User Auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -77,4 +77,13 @@ router.post('/login', (req, res) => {
     })
 })
 
+// Private Auth Route
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    handle: req.user.handle,
+    email: req.user.email
+  });
+})
+
 module.exports = router;
