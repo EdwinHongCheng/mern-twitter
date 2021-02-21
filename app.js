0861d196e468789fabddcad6b56319d7ffe377fe
Edwin Cheng <edwincheng3@gmail.com>
2021-02-21 02:31:54 -0800

    finish Express Routing Reading (aAO)

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+// [NOTE] ref: https://github.com/appacademy/mern-twitter/blob/master/app.js
+// - commented out code: not there yet
+
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+// const passport = require('passport');
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -10,5 +18,15 @@ mongoose
 
 
 app.get("/", (req, res) => res.send("Hello Kitty"));
+
+// app.use(passport.initialize());
+// require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
